6efea959449e6ba4437bd50838f8ad1ccfe55bb3
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-04-12 12:41:00 +0300

    updated

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -54,13 +54,13 @@ export default function ProductDetail() {
         <h3>Back to products</h3>
       </div>
 
-      <div class="py-6">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
-          <div class="flex flex-col md:flex-row -mx-4">
-            <div class="md:flex-1 px-4">
+      <div className="py-6">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
+          <div className="flex flex-col md:flex-row -mx-4">
+            <div className="md:flex-1 px-4">
               <div>
-                <div class="h-80 md:h-80 rounded-lg bg-gray-100 mb-4">
-                  <div class="h-[320px] md:h-[320px] lg:h-[320px] rounded-lg bg-gray-100 mb-4 flex items-center justify-center">
+                <div className="h-80 md:h-80 rounded-lg bg-gray-100 mb-4">
+                  <div className="h-[320px] md:h-[320px] lg:h-[320px] rounded-lg bg-gray-100 mb-4 flex items-center justify-center">
                     <img
                       className="w-[280px] rounded-lg cards"
                       src={product.image}
@@ -71,33 +71,37 @@ export default function ProductDetail() {
               </div>
             </div>
 
-            <div class="md:flex-1 px-4">
-              <h2 class="mb-2 leading-tight tracking-tight font-bold text-2xl md:text-3xl text-base-100">
+            <div className="md:flex-1 px-4">
+              <h2 className="mb-2 leading-tight tracking-tight font-bold text-2xl md:text-3xl text-base-100">
                 {product.name}
               </h2>
-              <p class="text-gray-500 text-sm">
+              <p className="text-gray-500 text-sm">
                 Brand: {""}
                 <span className="font-medium">{product.brand}</span>
               </p>
 
-              <div class="flex items-center space-x-4 my-4">
+              <div className="flex items-center space-x-4 my-4">
                 <div>
-                  <div class="rounded-lg bg-gray-100 flex py-2 px-3 ">
-                    <span class=" mr-1 mt-1">$</span>
-                    <span class="font-bold text-base-200 text-3xl">
+                  <div className="rounded-lg bg-gray-100 flex py-2 px-3 ">
+                    <span className=" mr-1 mt-1">$</span>
+                    <span className="font-bold text-base-200 text-3xl">
                       {product.price}
                     </span>
                   </div>
                 </div>
-                <div class="flex-1">
-                  <p class="text-green-500 text-xl font-semibold">Save 12%</p>
-                  <p class="text-gray-400 text-sm">Inclusive of all Taxes.</p>
+                <div className="flex-1">
+                  <p className="text-green-500 text-xl font-semibold">
+                    Save 12%
+                  </p>
+                  <p className="text-gray-400 text-sm">
+                    Inclusive of all Taxes.
+                  </p>
                 </div>
               </div>
 
-              <p class="text-gray-500">{product.desc}</p>
+              <p className="text-gray-500">{product.desc}</p>
 
-              <div class="flex py-2 ">
+              <div className="flex py-2 ">
                 <div className="mt-4 card card-compact dropdown-content w-52 bg-base-100 shadow flex flex-col  sm:w-[200px] sm:mx-20 h-[150px]">
                   <div className="card-body text-white">
                     <div className="flex items-center gap-24 px-1 sm:px-0 justify-between">
@@ -113,7 +117,9 @@ export default function ProductDetail() {
                     </div>
 
                     <button
-                      onClick={addToCart}
+                      onClick={() => {
+                        addToCart;
+                      }}
                       className="my-auto btn  bg-gray-200 text-base-100 active:text-white hover:text-white"
                     >
                       Add to Cart
@@ -129,25 +135,25 @@ export default function ProductDetail() {
   );
 }
 
-{
-  /* <div className="mt-6 card card-compact dropdown-content w-52 bg-base-100 shadow flex flex-col mx-auto sm:w-[200px] sm:mx-20 h-[180px]">
-  <div className="card-body text-white">
-    <div className="flex items-center gap-24 px-1 sm:px-0 justify-between">
-      <h3>Price</h3>
-      <p>${product.price}</p>
-    </div>
+// {
+//   /* <div className="mt-6 card card-compact dropdown-content w-52 bg-base-100 shadow flex flex-col mx-auto sm:w-[200px] sm:mx-20 h-[180px]">
+//     <div className="card-body text-white">
+//       <div className="flex items-center gap-24 px-1 sm:px-0 justify-between">
+//         <h3>Price</h3>
+//         <p>${product.price}</p>
+//       </div>
 
-    <div className="flex items-center gap-20 px-1 justify-between sm:px-0">
-      <h3>Status</h3>
-      <p>{product.countInStock > 0 ? "In Stock" : "Unavaliable"}</p>
-    </div>
+//       <div className="flex items-center gap-20 px-1 justify-between sm:px-0">
+//         <h3>Status</h3>
+//         <p>{product.countInStock > 0 ? "In Stock" : "Unavaliable"}</p>
+//       </div>
 
-    <button
-      onClick={addToCart}
-      className="my-auto btn  bg-gray-200 text-base-100 active:text-white hover:text-white"
-    >
-      Add to Cart
-    </button>
-  </div>
-</div>; */
-}
+//       <button
+//         onClick={addToCart}
+//         className="my-auto btn  bg-gray-200 text-base-100 active:text-white hover:text-white"
+//       >
+//         Add to Cart
+//       </button>
+//     </div>
+//   </div>; */
+// }

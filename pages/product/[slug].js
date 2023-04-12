10296dad04f5d717f4dc52f93972507ac9f165c0
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-04-12 12:16:49 +0300

    updated

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -36,7 +36,7 @@ export default function ProductDetail() {
     }
 
     dispatch({ type: "ADD_TO_CART", payload: { ...product, quantity } });
-    router.push('/cart')
+    router.push("/cart");
   };
 
   return (
@@ -45,66 +45,109 @@ export default function ProductDetail() {
         <title>Product Detail</title>
       </Head>
 
-      <div className="max-w-[500px] sm:max-w-[450px] md:max-w-[650px] lg:max-w-[900px] ">
-        <div className="font-semibold flex items-center gap-1 my-4">
-          <button className="btn btn-ghost">
-            <Link href={`/`}>
-              <HiChevronLeft size={30} />
-            </Link>
-          </button>
-          <h3>Back to products</h3>
-        </div>
+      <div className="font-semibold flex items-center gap-1 my-4">
+        <button className="btn btn-ghost">
+          <Link href={`/`}>
+            <HiChevronLeft size={30} />
+          </Link>
+        </button>
+        <h3>Back to products</h3>
+      </div>
 
-        <div className="max-w-[400px] mt-12 sm:max-w-[900px] sm:grid sm:grid-cols-3 gap-20 sm:w-full sm:mx-auto md:mx-auto lg:mx-auto ">
-          <div className="mx-auto sm:mt-6 flex justify-center items-center sm:w-[200px]">
-            <img
-              className="rounded-md sm:w-[300px]"
-              src={product.image}
-              alt={product.name}
-            />
-          </div>
+      <div class="py-6">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
+          <div class="flex flex-col md:flex-row -mx-4">
+            <div class="md:flex-1 px-4">
+              <div>
+                <div class="h-80 md:h-80 rounded-lg bg-gray-100 mb-4">
+                  <div class="h-[320px] md:h-[320px] lg:h-[320px] rounded-lg bg-gray-100 mb-4 flex items-center justify-center">
+                    <img
+                      className="w-[280px] rounded-lg cards"
+                      src={product.image}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
 
-          <div className="flex flex-col mx-auto justify-center  gap-4 mt-4 sm:w-[230px] sm:mx-6 text-center sm:text-start">
-            <h3 className="text-base-100 font-bold ">{product.name}</h3>
-            <h3>
-              <span className="text-base-100 font-bold ">Catagory:</span>{" "}
-              {product.catagory}
-            </h3>
-            <h3>
-              <span className="text-base-100 font-bold ">Brand: </span>{" "}
-              {product.brand}
-            </h3>
-            <h3>
-              {product.rating} of {product.numReviews} reviews
-            </h3>
-            <h3>
-              <span className="text-base-100 font-bold ">Description: </span>{" "}
-              {product.desc}
-            </h3>
-          </div>
+            <div class="md:flex-1 px-4">
+              <h2 class="mb-2 leading-tight tracking-tight font-bold text-2xl md:text-3xl text-base-100">
+                {product.name}
+              </h2>
+              <p class="text-gray-500 text-sm">
+                Brand: {""}
+                <span className="font-medium">{product.brand}</span>
+              </p>
 
-          <div className="mt-6 card card-compact dropdown-content w-52 bg-base-100 shadow flex flex-col mx-auto sm:w-[200px] sm:mx-20 h-[180px]">
-            <div className="card-body text-white">
-              <div className="flex items-center gap-24 px-1 sm:px-0 justify-between">
-                <h3>Price</h3>
-                <p>${product.price}</p>
+              <div class="flex items-center space-x-4 my-4">
+                <div>
+                  <div class="rounded-lg bg-gray-100 flex py-2 px-3 ">
+                    <span class=" mr-1 mt-1">$</span>
+                    <span class="font-bold text-base-200 text-3xl">
+                      {product.price}
+                    </span>
+                  </div>
+                </div>
+                <div class="flex-1">
+                  <p class="text-green-500 text-xl font-semibold">Save 12%</p>
+                  <p class="text-gray-400 text-sm">Inclusive of all Taxes.</p>
+                </div>
               </div>
 
-              <div className="flex items-center gap-20 px-1 justify-between sm:px-0">
-                <h3>Status</h3>
-                <p>{product.countInStock > 0 ? "In Stock" : "Unavaliable"}</p>
-              </div>
+              <p class="text-gray-500">{product.desc}</p>
 
-              <button
-                onClick={addToCart}
-                className="my-auto btn  bg-gray-200 text-base-100 active:text-white hover:text-white"
-              >
-                Add to Cart
-              </button>
+              <div class="flex py-2 ">
+                <div className="mt-4 card card-compact dropdown-content w-52 bg-base-100 shadow flex flex-col  sm:w-[200px] sm:mx-20 h-[150px]">
+                  <div className="card-body text-white">
+                    <div className="flex items-center gap-24 px-1 sm:px-0 justify-between">
+                      <h3>Price</h3>
+                      <p>${product.price}</p>
+                    </div>
+
+                    <div className="flex items-center gap-20 px-1 justify-between sm:px-0">
+                      <h3>Status</h3>
+                      <p>
+                        {product.countInStock > 0 ? "In Stock" : "Unavaliable"}
+                      </p>
+                    </div>
+
+                    <button
+                      onClick={addToCart}
+                      className="my-auto btn  bg-gray-200 text-base-100 active:text-white hover:text-white"
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
+}
+
+{
+  /* <div className="mt-6 card card-compact dropdown-content w-52 bg-base-100 shadow flex flex-col mx-auto sm:w-[200px] sm:mx-20 h-[180px]">
+  <div className="card-body text-white">
+    <div className="flex items-center gap-24 px-1 sm:px-0 justify-between">
+      <h3>Price</h3>
+      <p>${product.price}</p>
+    </div>
+
+    <div className="flex items-center gap-20 px-1 justify-between sm:px-0">
+      <h3>Status</h3>
+      <p>{product.countInStock > 0 ? "In Stock" : "Unavaliable"}</p>
+    </div>
+
+    <button
+      onClick={addToCart}
+      className="my-auto btn  bg-gray-200 text-base-100 active:text-white hover:text-white"
+    >
+      Add to Cart
+    </button>
+  </div>
+</div>; */
 }

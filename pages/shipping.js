c361239b1b6c055c4892f0704762b5ad15b23015
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-04-17 13:23:32 +0300

    updated

--- pages/shipping.js
+++ pages/shipping.js
@@ -21,19 +21,7 @@ export default function shipping() {
       <div>
         <CheckoutWizard activeStep={1} />
 
-        <section class="shadow-blue-100 mx-auto max-w-screen-lg rounded-xl bg-white text-gray-600 shadow-lg sm:my-10 sm:border">
-          <div class="container mx-auto flex flex-col flex-wrap px-5 pb-12">
-            <div class="flex w-full flex-col">
-              <h1 class="text-2xl font-semibold">Meal Preferences</h1>
-
-              <div class="flex flex-col justify-between sm:flex-row">
-                <button class="group order-1 my-2 flex w-full items-center justify-center rounded-lg bg-gray-200 py-2 text-center font-bold text-gray-600 outline-none transition sm:w-40 focus:ring hover:bg-gray-300">
-                  Next
-                </button>
-              </div>
-            </div>
-          </div>
-        </section>
+        <form></form>
       </div>
     </div>
   );
